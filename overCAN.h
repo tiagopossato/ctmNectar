f442f18c2e08472ece8f428d02ab25eecff5ef74
Tiago Possato <tiago.possato@ifc-videira.edu.br>
2017-02-14 22:29:21 -0200

    Incluidas mais funcionalidades

--- overCAN.h
+++ overCAN.h
@@ -4,21 +4,30 @@
 #ifndef overCAN_h_
 #define overCAN_h_
 
-#define ONLINE  1
+#define ONLINE 1
 #define SEND_DATA 2
 #define IS_ONLINE 3
 
 #define SEND_CONFIG 10
 #define CHANGE_ID 11
-#define CHANGE_READ_TIME  12
-#define CHANGE_SEND_TIME  13
+#define CHANGE_READ_TIME 12
+#define CHANGE_SEND_TIME 13
 
 #define SEND_TIME 21
+#define SEND_READ_TIME 22
 
 #define CHANGE_OUTPUT_STATE 50
 
-#define OUTPUT_1_STATE  51
+#define OUTPUT_1_STATE 51
 
-#define INPUT_1_STATE  61
+#define INPUT_1_STATE 61
+
+#define ANALOG_VALUE 70
+
+#define TEMPERATURA 71
+
+#define UMIDADE_AR 72
+
+#define UMIDADE_SOLO 73
 
 #endif
