7aa57d4d30d71240237768a0dcb1f170a07d2f48
Tiago Possato <tiago.possato@yahoo.com.br>
2017-02-18 16:32:50 -0200

    Alterado nome

---
+++ ctmNectar.h
@@ -0,0 +1,26 @@
+/**
+   Definições dos códigos de comunicação
+*/
+#ifndef ctmNectar_h_
+#define ctmNectar_h_
+
+#define ONLINE 1
+#define SEND_DATA 2
+#define IS_ONLINE 3
+
+#define SEND_CONFIG 10
+#define CHANGE_ID 11
+#define CHANGE_READ_TIME 12
+#define CHANGE_SEND_TIME 13
+
+#define SEND_TIME 21
+
+#define CHANGE_OUTPUT_STATE 50
+
+#define OUTPUT_1_STATE 51
+
+#define INPUT_1_STATE 61
+
+#define ANALOG_VALUE 71
+
+#endif
